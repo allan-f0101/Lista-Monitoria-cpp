99fdfbe00fb3e9b952eb8c92b0d858275138ae1d
SuPeR <allandev472@gmail.com>
2026-06-13 15:48:28 -0300

    Questão A mod

--- Lista 2 VJudge/A.cpp
+++ Lista 2 VJudge/A.cpp
@@ -9,9 +9,24 @@ private:
     enum Estado{VAZIO, OCUPADO, DELETADO};
 
     static const int tam = 101;
+    string keys[tam];
     E values[tam];
     Estado estado[tam];
     int cont;
+
+    int Hash(string key){
+        int forCont = 1;
+        int sum = 0;
+
+        for(char i : key){
+            sum += i * forCont;
+            forCont++;
+        }
+
+        int function = 19 * sum;
+        
+        return function % 101;
+    };
 
 public:
     Dictionary() : cont(0) {
@@ -21,7 +36,16 @@ public:
     }
     ~Dictionary();
 
-    void insert(string key, E value);
+    void insert(string key, E value){
+        int initialPos = Hash(key);
+        
+        for(int i = 0; i < 20; i++){
+            if(initialPos == 0){
+                
+            }
+        }
+    }
+
     E remove(string key);
     E find(string key);
     void clear();
