db11156c2488bfffe25f82530fcc2240f12f512b
afl4 <afl4@g2c28.cin.ufpe.br>
2026-06-16 12:03:05 -0300

    Problema B

---
+++ Lista 2 VJudge/B.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <list>
+
+template<typename E>
+class Dictionary{
+private:
+    struct Par{
+        string chave;
+        E value;
+    };
+
+    static const int tam = 100003;
+
+    list<Par> lista[tam];
+
+    int Hash(string key){
+        int forCont = 1;
+        int sum = 0;
+
+        for(char i : key){
+            sum += i * forCont;
+            forCont++;
+        }
+
+        return sum % tam;
+    }
+
+    int cont;
+
+public:
+    Dictionary() : cont(0){
+        for(int i = 0; i < tam; i++){
+            lista[i].clear();
+        }
+    }
+    ~Dictionary(){}
+
+    void insert(string key, E value){
+        int pos = Hash(key);
+
+        Par novoPar;
+        novoPar.chave = key;
+        novoPar.value = value;
+
+        lista[pos].push_back(novoPar);
+
+        cont++;
+    }
+
+    E find(string key){
+        int pos = Hash(key);
+
+        for(auto const& par : lista[pos]){
+            if(par.chave == key){
+                return par.value;
+            }
+        }
+
+        return E();
+    }
+
+    void clear(){
+        for(int i = 0; i < tam; i++){
+            
+        }
+    }
+
+    int size(){
+        return cont;
+    }
+};
+
+int main(){
+    return 0;
+}
